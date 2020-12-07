fb4d6c18bc93446a91188b114ac6c78c8c139720
Greg <gregsentai@gmail.com>
2020-12-07 11:47:37 -0500

    tournament class created

---
+++ dotnet/Capstone/Controllers/TournamentController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Capstone.DAO;
+using Capstone.Models;
+using Capstone.Security;
+using Microsoft.AspNetCore.Authorization;
+
+//namespace Capstone.Controllers
+//{
+//    [Route("[controller]/create-tournament")]
+//    [ApiController]
+//    public class TournamentController : ControllerBase
+//    {
+
+//        [HttpPost]
+//        public IActionResult CreateTournament(Tournament newTournament)
+//        {
+//            IActionResult result;
+
+//            User existingUser = userDAO.GetUser(userParam.Username);
+//            if (existingUser != null)
+//            {
+//                return Conflict(new { message = "Username already taken. Please choose a different username." });
+//            }
+
+//            User user = userDAO.AddUser(userParam.Username, userParam.Password, userParam.Role, userParam.Email);
+//            if (user != null)
+//            {
+//                result = Created(user.Username, null); //values aren't read on client
+//            }
+//            else
+//            {
+//                result = BadRequest(new { message = "An error occurred and user was not created." });
+//            }
+
+//            return result;
+//        }
+//    }
+//}
---
+++ dotnet/Capstone/Models/Tournament.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Models
+{
+    public class Tournament
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public bool InPerson { get; set; }
+        public int ZipCode { get; set; }
+        public string Link { get; set; }
+        public string Size { get; set; }
+        public string Style { get; set; }
+        public string MatchStyle { get; set; }
+        public int MatchSize { get; set; }
+        public string Description { get; set; }
+        public DateTime RegistrationClosedDate { get; set; }
+        public DateTime StartDate { get; set;}
+        public string RegistrationType { get; set; }
+    }
+}
